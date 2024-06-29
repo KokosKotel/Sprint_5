e2571b31c50d47dfcfd0a5982671b88d30110168
Denis Dudin <kotel.kokos@yandex.ru>
2024-06-29 10:22:27 +0300

    Dudin_Denis Sprint_5 corrected

--- test/test_navigation_to_constructs.py
+++ test/test_navigation_to_constructs.py
@@ -13,7 +13,7 @@ class TestNavigationConstructs:
         driver.find_element(*Locators.SAUCES_TEXT).click()
         driver.find_element(*Locators.BUNS_TEXT).click()
         buns_tab = driver.find_element(*Locators.SELECTED_SECTION)
-        assert buns_tab.is_displayed()
+        assert buns_tab.text == "Булки"
 
     #Тест перехода в раздел "Соусы"
     def test_navigation_to_sauces_section(self, login):
@@ -23,7 +23,7 @@ class TestNavigationConstructs:
         )))
         driver.find_element(*Locators.SAUCES_TEXT).click()
         sauces_tab = driver.find_element(*Locators.SELECTED_SECTION)
-        assert sauces_tab.is_displayed()
+        assert sauces_tab.text == "Соусы"
 
     #Тест перехода в раздел "Начинка"
     def test_navigation_to_fillings_section(self, login):
@@ -33,4 +33,4 @@ class TestNavigationConstructs:
         )))
         driver.find_element(*Locators.FILLING_TEXT).click()
         fillings_tab = driver.find_element(*Locators.SELECTED_SECTION)
-        assert fillings_tab.is_displayed()
+        assert fillings_tab.text == "Начинки"
